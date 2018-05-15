fe137b81d91429956ebee62e13da491641d0a108
Tom DAGENS <dagens.t@int.univ-amu.fr>
2018-05-15 18:10:43 +0200

    litle tweaks

--- onsets_to_spm.m
+++ onsets_to_spm.m
@@ -1,48 +1,48 @@
 load('dm_run1.mat', 'onsets' )
 
-filePh = fopen('log_Subj1_run1_onsets.txt','w');
+filePh = fopen('log_Subj2_run1_onsets.txt','w');
 fprintf(filePh,'%f\n',onsets{:});
 fclose(filePh);
 
 load('dm_run2.mat', 'onsets' )
 
-filePh = fopen('log_Subj1_run2_onsets.txt','w');
+filePh = fopen('log_Subj2_run2_onsets.txt','w');
 fprintf(filePh,'%f\n',onsets{:});
 fclose(filePh);
 
 load('dm_run3.mat', 'onsets' )
 
-filePh = fopen('log_Subj1_run3_onsets.txt','w');
+filePh = fopen('log_Subj2_run3_onsets.txt','w');
 fprintf(filePh,'%f\n',onsets{:});
 fclose(filePh);
 
 load('dm_run4.mat', 'onsets' )
 
-filePh = fopen('log_Subj1_run4_onsets.txt','w');
+filePh = fopen('log_Subj2_run4_onsets.txt','w');
 fprintf(filePh,'%f\n',onsets{:});
 fclose(filePh);
 
 load('dm_run5.mat', 'onsets' )
 
-filePh = fopen('log_Subj1_run5_onsets.txt','w');
+filePh = fopen('log_Subj2_run5_onsets.txt','w');
 fprintf(filePh,'%f\n',onsets{:});
 fclose(filePh);
 
 load('dm_run6.mat', 'onsets' )
 
-filePh = fopen('log_Subj1_run6_onsets.txt','w');
+filePh = fopen('log_Subj2_run6_onsets.txt','w');
 fprintf(filePh,'%f\n',onsets{:});
 fclose(filePh);
 
 load('dm_run7.mat', 'onsets' )
 
-filePh = fopen('log_Subj1_run7_onsets.txt','w');
+filePh = fopen('log_Subj2_run7_onsets.txt','w');
 fprintf(filePh,'%f\n',onsets{:});
 fclose(filePh);
 
 load('dm_run8.mat', 'onsets' )
 
-filePh = fopen('log_Subj1_run8_onsets.txt','w');
+filePh = fopen('log_Subj2_run8_onsets.txt','w');
 fprintf(filePh,'%f\n',onsets{:});
 fclose(filePh);
 
