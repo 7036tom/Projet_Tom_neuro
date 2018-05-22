efafe304e8981453db816cd29efe703d8afca770
Tom DAGENS <dagens.t@int.univ-amu.fr>
2018-05-22 13:03:35 +0200

    corrections

--- Mean_Anat.m
+++ Mean_Anat.m
@@ -5,18 +5,18 @@
 %-----------------------------------------------------------------------
 %%
 matlabbatch{1}.spm.spatial.realign.write.data = {
-                                                 'path/Subj3/Anat/wrhighres001.nii,1'
-                                                 'path/Subj1/Anat/wrhighres001.nii,1'
-                                                 'path/Subj5/Anat/wrhighres001.nii,1'
-                                                 'path/Subj6/Anat/wrhighres001.nii,1'
-                                                 'path/Subj7/Anat/wrhighres001.nii,1'
-                                                 'path/Subj8/Anat/wrhighres001.nii,1'
-                                                 'path/Subj9/Anat/wrhighres001.nii,1'
-                                                 'path/Subj10/Anat/wrhighres001.nii,1'
-                                                 'path/Subj11/Anat/wrhighres001.nii,1'
-                                                 'path/Subj12/Anat/wrhighres001.nii,1'
-                                                 'path/Subj2/Anat/wrhighres001.nii,1'
-                                                 'path/Subj4/Anat/wrhighres001.nii,1'
+                                                 '/hpc/banco/dagens.t/scripts_utiles/Neuro/Subj3/Anat/wrhighres001.nii,1'
+                                                 '/hpc/banco/dagens.t/scripts_utiles/Neuro/Subj1/Anat/wrhighres001.nii,1'
+                                                 '/hpc/banco/dagens.t/scripts_utiles/Neuro/Subj5/Anat/wrhighres001.nii,1'
+                                                 '/hpc/banco/dagens.t/scripts_utiles/Neuro/Subj6/Anat/wrhighres001.nii,1'
+                                                 '/hpc/banco/dagens.t/scripts_utiles/Neuro/Subj7/Anat/wrhighres001.nii,1'
+                                                 '/hpc/banco/dagens.t/scripts_utiles/Neuro/Subj8/Anat/wrhighres001.nii,1'
+                                                 '/hpc/banco/dagens.t/scripts_utiles/Neuro/Subj9/Anat/wrhighres001.nii,1'
+                                                 '/hpc/banco/dagens.t/scripts_utiles/Neuro/Subj10/Anat/wrhighres001.nii,1'
+                                                 '/hpc/banco/dagens.t/scripts_utiles/Neuro/Subj11/Anat/wrhighres001.nii,1'
+                                                 '/hpc/banco/dagens.t/scripts_utiles/Neuro/Subj12/Anat/wrhighres001.nii,1'
+                                                 '/hpc/banco/dagens.t/scripts_utiles/Neuro/Subj2/Anat/wrhighres001.nii,1'
+                                                 '/hpc/banco/dagens.t/scripts_utiles/Neuro/Subj4/Anat/wrhighres001.nii,1'
                                                  };
 %%
 matlabbatch{1}.spm.spatial.realign.write.roptions.which = [0 1];
